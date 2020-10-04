078f7ba99902210571232aaceaf7b5164fa62c91
MatthewsTomts <71537027+MatthewsTomts@users.noreply.github.com>
2020-10-03 21:18:54 -0300

    agr esse vai

---
+++ VetorAgrVai/src/vetoragrvai/VetorAgrVai.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetoragrvai;
+
+import java.util.Arrays;
+import java.util.Calendar;
+
+/**
+ *
+ * @author learn
+ */
+public class VetorAgrVai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[] = {3, 2, 8, 7, 5, 4}; //ou int[] n = new int[6];
+        for (int c = 0; c < n.length; c++) {
+            System.out.println("Na posição "+ c + ", temos o valor " + n[c]+ " ");
+        }
+        
+        Calendar ano = Calendar.getInstance();
+        int anoA = ano.get(Calendar.YEAR);
+        String[] mes = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+            "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+        int[] tot = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for (int c = 0; c < mes.length; c++) {
+            if (anoA % 4 == 0 && anoA % 100 != 0 || anoA % 400 == 0) {
+                tot[1] = 29;
+            }
+            System.out.println("O Mês " + mes[c] + " tem " + tot[c] + " Dias");
+        }
+        
+        double num[] = {3.5, 2.75, 9, -4.5};
+        int test[] = new int[20];
+        
+        int pos = Arrays.binarySearch(num, 9); //Procura pela posição do 9 no vetor num
+        Arrays.sort(num); //Organiza os números no vetor num
+        Arrays.fill(test, 0); //Preenche o vetor Test com zeros
+        
+        System.out.println("Encontrei o 9 na posição " + pos + " do vetor num");
+        
+        for (double valor: num) { 
+            /*valor recebe os valores de num de acordo com a ordem
+            e quando chega no final do num ele para
+            Funciona apenas em coleções/vetores*/
+            System.out.print(valor + ", ");
+        }
+        
+        System.out.println("");
+        for (int valor: test) {
+            System.out.print(valor + " ");
+        }
+    }
+    
+}
